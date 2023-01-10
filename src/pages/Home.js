ddb6605bdcb4c9c7e30062561c4e390f87910db1
bk <btko@tuta.io>
2023-01-10 20:35:21 +0100

    starting to plan folder structure and removing stuff

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>This will be a home page soon</h1>
+      </div>
+    );
+  }
+}
