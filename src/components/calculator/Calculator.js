019ebd62519b87bbac40454f295d75280a425c1a
btk44 <btko@tuta.io>
2023-01-13 11:49:48 +0100

    much better look

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,13 +1,8 @@
 import './Calculator.css'
 
 export default function Calculator() {
-  const mathSymbols = ['/','x','-','+','=']
-  const buttonsTable = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['.', '0', 'C']
-  ]
+  const mathSymbols = ['÷','×','−','+','=']
+  const numbers = ['7','8','9','4','5','6','1','2','3','.','0','C']
 
   function buttonClicked(event){
     console.log(event.target.innerHTML)
@@ -15,22 +10,20 @@ export default function Calculator() {
   
   return (
     <div className='calculator-component'>
-      <div className='numeric-buttons-column'>
-        { buttonsTable.map(buttonRow =>
-            <div className='buttons-row'>
-              { buttonRow.map(buttonText => 
-                  <button className='button numeric-button' onClick={buttonClicked}>
-                    { buttonText }
-                  </button>
-                )
-              }
-            </div>
+      <div className='display-text'>
+        <input type='text'></input>
+      </div>
+      <div className='numeric-buttons'>
+        { numbers.map(buttonText =>
+            <button key={buttonText} className='button numeric-button' onClick={buttonClicked}>
+              { buttonText }
+            </button>
           )
         }
       </div>
-      <div className='math-buttons-column'>
+      <div className='math-buttons'>
         { mathSymbols.map(buttonText => 
-            <button className='button math-button' onClick={buttonClicked}>
+            <button key={buttonText} className='button math-button' onClick={buttonClicked}>
               { buttonText }
             </button>
           )
