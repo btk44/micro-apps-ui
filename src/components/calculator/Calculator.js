fad68a76c60e064e60ccebcf99f6b9a620b8d5d5
bk <btko@tuta.io>
2023-01-16 20:51:03 +0100

    events testing

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,22 +1,15 @@
-import { useState } from 'react'
 import './Calculator.css'
 import MathSybols from './MathSymbols'
 
-export default function Calculator() {
+export default function Calculator({resultChanged}) {
   const clearSymbol = 'C'
   const mathSymbols = [MathSybols.Obelus, MathSybols.Asterisk, MathSybols.Minus,
                        MathSybols.Plus, MathSybols.Equal]
   const numbers = ['7','8','9','4','5','6','1','2','3',
-                   MathSybols.Dot,MathSybols.Zero,clearSymbol]
-
-  const [result, setResult] = useState(0);                 
+                   MathSybols.Dot,MathSybols.Zero,clearSymbol]      
 
   function buttonClicked(event){
-    console.log(event.target.innerHTML)
-  }
-
-  function resultChanged(){
-
+    resultChanged(event.target.innerHTML)
   }
   
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,15 @@ import React from "react";
 import Calculator from "../components/calculator/Calculator";
 
 export default class Home extends React.Component {
+  testHandler(value){
+    console.log('home ' + value)
+  }
+
   render() {
     return (
       <div className="home">
         <h1>This will be a home page soon</h1>
-        <Calculator></Calculator>
+        <Calculator resultChanged={this.testHandler}></Calculator>
       </div>
     );
   }
