c2d6b54b7618352ab150651e969d4eee4dd70f09
btk44 <btko@tuta.io>
2023-01-19 15:03:04 +0100

    category-piscker changes

--- src/components/category-picker/CategoryPicker.js
+++ src/components/category-picker/CategoryPicker.js
@@ -5,12 +5,23 @@ export default function CategoryPicker({initialValue, updateValue}) {
   
   const categories = DummyCategories
 
-  return (
-    categories.map(category => 
-        <div className='category' key={category.id}>
-            <img className='icon' src={require('./icons/icon.svg').default} alt='' style={{borderColor: category.color}}/>
-            <div className='description'>{ category.name }</div>
-        </div>
-    )
-  )
+  function onCategorySelect(categoryId){
+    
+  }
+
+  function getCategoryList(parentId){
+    return (
+        <ul className='category-picker'>
+          { categories.filter(category => category.parentId === parentId).map(category => 
+              <li className='category' key={category.id}>
+                <img className='icon' src={require('./icons/icon.svg').default} alt='' style={{borderColor: category.color}}/>
+                <div className='description'>{ category.name }</div>
+              </li>
+            )
+          }
+        </ul>
+      )
+  }
+
+  return getCategoryList(0)
 }
