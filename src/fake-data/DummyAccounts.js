a0cdfb8ac97500e4648019198ed9184145040a96
btk44 <btko@tuta.io>
2023-02-02 14:51:30 +0100

    more styling

--- src/fake-data/DummyAccounts.js
+++ src/fake-data/DummyAccounts.js
@@ -1,14 +1,14 @@
 const DummyAccounts = [
-    { id: 1, name: 'account 1', color: '#347aeb', icon: 'sth.svg'},
-    { id: 2, name: 'account 2', color: '#347aeb', icon: 'sth.svg' },
-    { id: 3, name: 'account 3', color: '#347aeb', icon: 'sth.svg' },
-    { id: 4, name: 'account 4', color: '#347aeb', icon: 'sth.svg' },
-    { id: 5, name: 'account 5', color: '#347aeb', icon: 'sth.svg' },
-    { id: 6, name: 'account 6', color: '#347aeb', icon: 'sth.svg' },
-    { id: 7, name: 'account 7', color: '#347aeb', icon: 'sth.svg' },
-    { id: 8, name: 'account 8', color: '#347aeb', icon: 'sth.svg' },
-    { id: 9, name: 'account 9', color: '#347aeb', icon: 'sth.svg' },
-    { id: 10, name: 'account 10', color: '#347aeb', icon: 'sth.svg' },
+    { id: 1, name: 'account 1', color: '#347aeb', icon: 'sth.svg', currency: 'PLN'},
+    { id: 2, name: 'account 2', color: '#347aeb', icon: 'sth.svg', currency: 'PLN' },
+    { id: 3, name: 'account 3', color: '#347aeb', icon: 'sth.svg', currency: 'EUR' },
+    { id: 4, name: 'account 4', color: '#347aeb', icon: 'sth.svg', currency: 'EUR' },
+    { id: 5, name: 'account 5', color: '#347aeb', icon: 'sth.svg', currency: 'CHF' },
+    { id: 6, name: 'account 6', color: '#347aeb', icon: 'sth.svg', currency: 'USD' },
+    { id: 7, name: 'account 7', color: '#347aeb', icon: 'sth.svg', currency: 'USD' },
+    { id: 8, name: 'account 8', color: '#347aeb', icon: 'sth.svg', currency: 'PLN' },
+    { id: 9, name: 'account 9', color: '#347aeb', icon: 'sth.svg', currency: 'PLN' },
+    { id: 10, name: 'account 10', color: '#347aeb', icon: 'sth.svg', currency: 'PLN' },
 ]
 
 export default DummyAccounts
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,84 @@
-import Calculator from "../components/calculator/Calculator";
-import { useState } from "react";
-import { CategoriesTree } from "../fake-data/DummyCategories";
-import ItemPicker from "../components/item-picker/ItemPicker";
-import DummyAccounts from "../fake-data/DummyAccounts";
+import Calculator from '../components/calculator/Calculator';
+import { useState } from 'react';
+import { CategoriesTree } from '../fake-data/DummyCategories';
+import ItemPicker from '../components/item-picker/ItemPicker';
+import DummyAccounts from '../fake-data/DummyAccounts';
 import './Home.scss'
 
-
+const TransactionType = {
+  Expense: 'exp',
+  Income: 'inc',
+  Transfer: 'tra' // maybe in the future
+}
 
 export default function Home() {
-  const [windows, setWindows] = useState({
+  const [pageLayout, setPageLayout] = useState({
     showCategoryPicker: false,
-    showAccountPicker: false
+    showAccountPicker: false,
   })
 
   const [transaction, setTransaction] = useState({
     accountId: 0,
     categoryId: 0,
-    amount: 0
+    amount: 0,
+    date: new Date(),
+    type: TransactionType.Expense
   })
 
   function showModal(){
-    return windows.showAccountPicker || windows.showCategoryPicker
+    return pageLayout.showAccountPicker || pageLayout.showCategoryPicker
   }
 
-  function closeWindows(){
-    setWindows({ showAccountPicker: false, showCategoryPicker: false })
+  function closeModal(){
+    setPageLayout({ ...pageLayout, showAccountPicker: false, showCategoryPicker: false })
+  }
+
+  function showCategoryPick(){
+    setPageLayout({ ...pageLayout, showCategoryPicker: true })
+  }
+
+  function showAccountPick(){
+    setPageLayout({ ...pageLayout, showAccountPicker: true })
   }
 
   function onAmountChange(amount){
     setTransaction({ ...transaction, amount: amount })
   }
 
+  function onTypeChange(type){
+    setTransaction({ ...transaction, type: type })
+  }
+
   function onAccountChange(account){
     setTransaction({ ...transaction, accountId: account.id })
-    closeWindows()
+    closeModal()
   }
 
   function categoryChange(category){
     setTransaction({ ...transaction, categoryId: category.id })
-    closeWindows()
+    closeModal()
   }
 
-  function showCategoryPick(){
-    setWindows({ showAccountPicker: false, showCategoryPicker: true })
-  }
+  function getDateString() {
+    const year = transaction.date.getFullYear()
+    let day = (transaction.date.getDate()).toString()
+    let month = (transaction.date.getMonth()+1).toString()
 
-  function showAccountPick(){
-    setWindows({ showAccountPicker: true, showCategoryPicker: false })
+    day = day.length < 2 ? `0${day}` : day
+    month = month.length < 2 ? `0${month}` : month
+
+    return `${year}/${month}/${day}`
   }
 
   return (
-    <div className="home">
+    <div className='home'>
       <div className='buttons'>
+        <button>{ 'Ania' }</button>
+        <button> { getDateString() }</button>
+        <button className={transaction.type !== TransactionType.Expense ? 'inactive' : ''}
+                onClick={() => onTypeChange(TransactionType.Expense)}>expense</button>
+        <button className={transaction.type !== TransactionType.Income ? 'inactive' : ''}
+                onClick={() => onTypeChange(TransactionType.Income)}>income</button>
         <button onClick={showCategoryPick}>{ transaction.categoryId ? transaction.categoryId : 'category' }</button>
         <button onClick={showAccountPick}> { transaction.accountId ? transaction.accountId : 'account' }</button>
       </div>
@@ -60,19 +87,19 @@ export default function Home() {
         updateValue={onAmountChange}></Calculator> }
       { showModal() &&
         <div className='modal'>
-          { windows.showCategoryPicker &&
+          { pageLayout.showCategoryPicker &&
             <ItemPicker 
               onUpdate={categoryChange}
-              onCancel={closeWindows}
+              onCancel={closeModal}
               sourceItemList={CategoriesTree}
               parentPropName='parentCategory'
               childListPropName='childCategories'
               keyPropName='id'></ItemPicker>
           }
-          { windows.showAccountPicker && 
+          { pageLayout.showAccountPicker && 
             <ItemPicker 
               onUpdate={onAccountChange}
-              onCancel={closeWindows}
+              onCancel={closeModal}
               sourceItemList={DummyAccounts}
               keyPropName='id'></ItemPicker>
           }
