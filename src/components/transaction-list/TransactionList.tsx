75fa03fa632f492b55858b0d827bd8f52fb1ded3
bk <btko@tuta.io>
2023-02-15 19:41:21 +0100

    transaction list started

--- src/components/transaction-list/TransactionList.tsx
+++ src/components/transaction-list/TransactionList.tsx
@@ -8,5 +8,16 @@ interface TransactionListProps{
 }
 
 export default function TransactionList(props: TransactionListProps) {
-    
-}
+  return (
+  <div className='transaction-list-component'>
+    <ul>
+      { props.transactions.map((transaction: Transaction) => 
+          <li key={transaction.id}>
+            <span>{ transaction.amount }</span>
+          </li> 
+        )
+      }
+    </ul>
+  </div>
+  )
+} 
--- src/objects/transaction.tsx
+++ src/objects/transaction.tsx
@@ -1,17 +1,21 @@
 import { TransactionType } from '../constants/transaction-type'
 
 export class Transaction {
+    id: number
     accountId: number
     categoryId: number
     amount: number
     date: Date
+    payee: string
     type: TransactionType   
   
     constructor() { // change it later
+      this.id = 0
       this.accountId = 0
       this.categoryId = 0
       this.amount = 0
       this.date = new Date()
+      this.payee= ''
       this.type = TransactionType.Expense    
     }
   }
