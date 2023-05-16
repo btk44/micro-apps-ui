6975e4a7ecc3730785c3f47eef93451ab56a7d51
btk44 <btko@tuta.io>
2023-05-16 19:24:05 +0200

    redux data fetching fix

--- src/store/TransactionSlice.tsx
+++ src/store/TransactionSlice.tsx
@@ -1,4 +1,4 @@
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { TransactionService } from '../services/transaction-service'
 import { Account } from '../objects/account'
 import { Category } from '../objects/category'
@@ -9,6 +9,7 @@ import { RootState } from './Store'
 // separate this store in the future if needed
 
 interface TransactionsState {
+  loading: boolean,
   accounts: Array<Account>,
   categories: Array<Category>,
   currencies: Array<Currency>,
@@ -21,51 +22,44 @@ interface TransactionLoadParams {
 }
 
 const initialState: TransactionsState = {
+  loading: false,
   accounts: [],
   categories: [],
   currencies: [],
   transactions: []
 }
 
+export const initialLoad = createAsyncThunk('transaction/initialLoad', async (parameters: TransactionLoadParams) => {
+      const transactionCall = TransactionService.SearchTransactions({ownerId: parameters.ownerId, take: parameters.transactionCount})
+      const accountsCall = TransactionService.SearchAccounts({ownerId: parameters.ownerId})
+      const categoryCall = TransactionService.SearchCategories({ownerId: parameters.ownerId})
+
+      return Promise
+        .all([transactionCall, accountsCall, categoryCall])
+        .then(([transactionsResponse, accountsResponse, categoriesResponse]) => {
+          const accountsDict: any = Object.assign({}, ...accountsResponse.map((x: Account) => ({[x.id]: x})));
+          const flattenCategories = categoriesResponse.concat(categoriesResponse.flatMap((x: Category) => x.subcategories))
+          const categoriesDict: any = Object.assign({}, ...flattenCategories.map((x: Category) => ({[x.id]: x})));
+          return { accounts: accountsDict, categories: categoriesDict, transactions: transactionsResponse, currencies: [] }
+        })
+})
+
 export const transactionSlice = createSlice({
   name: 'transaction',
   initialState,
-  reducers: {
-    initialLoad: (state: any, action: PayloadAction<TransactionLoadParams>) => {
-      const transactionCall = TransactionService.SearchTransactions({ownerId: action.payload.ownerId, 
-                                                                     take: action.payload.transactionCount})
-      const accountsCall = TransactionService.SearchAccounts({ownerId: action.payload.ownerId})
-      const categoryCall = TransactionService.SearchCategories({ownerId: action.payload.ownerId})
-
-      Promise.all([transactionCall, accountsCall, categoryCall])
-      .then(([transactionsResponse, accountsResponse, categoriesResponse]) => {
-         state.transactions = transactionsResponse
-
-         const accountsDict: any = {}
-         accountsResponse.forEach((x: Account) => {
-           accountsDict[x.id] = x
-         });
-         state.accounts = accountsDict
-
-         const categoriesDict: any = {}
-         const flattenCategories = categoriesResponse.concat(categoriesResponse.flatMap((x: Category) => x.subcategories))
-         flattenCategories.forEach((x: Category) => {
-           categoriesDict[x.id] = x
-         });
-         state.categories = categoriesDict
-      })
-      .catch(error => console.error(error))
-    }
+  reducers: { },
+  extraReducers: (builder) => {
+    builder.addCase(initialLoad.pending, (state: TransactionsState) => { state.loading = true })
+    builder.addCase(initialLoad.rejected, (state: TransactionsState) => { state.loading = false })
+    builder.addCase(initialLoad.fulfilled, (state: TransactionsState, action) => {  
+      state.loading = false
+      state.accounts = action.payload.accounts
+      state.categories = action.payload.categories
+      state.transactions = action.payload.transactions
+    })
   }
 })
 
-// export const fetchPosts = createAsyncThunk('transaction/initial', async () => {
-//   const response = await client.get('/fakeApi/posts')
-//   return response.data
-// })
-
-// Action creators are generated for each case reducer function
-export const { initialLoad } = transactionSlice.actions
 export const selectAccounts = (state: RootState) => state.transactionStore.accounts
 export const selectCategories = (state: RootState) => state.transactionStore.categories
 export const selectCurrencies = (state: RootState) => state.transactionStore.currencies
