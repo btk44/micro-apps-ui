40439322c7e76d2467f28e3ccbb76da926c81cf3
btk44 <btko@tuta.io>
2023-05-23 21:15:37 +0200

    local storage for redux

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -2,12 +2,28 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './UserSlice'
 import transactionReducer from './TransactionSlice'
 
+const saveStoreToLocalStorage = (store: any) => {
+  return (next: any) => (action: any) => {
+    const result = next(action);
+    localStorage.setItem('appState', JSON.stringify(store.getState()));
+    return result;
+  };
+};
+
+const preloadStoreFromLocalStorage = () => {
+  const storeStateString = localStorage.getItem('appState')
+  if (storeStateString !== null) {
+    return JSON.parse(storeStateString); 
+  }
+};
 
 const store = configureStore({
   reducer: {
     userStore: userReducer,
     transactionStore: transactionReducer  
   },
+  preloadedState: preloadStoreFromLocalStorage(),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(saveStoreToLocalStorage)
 })
 
 export default store
