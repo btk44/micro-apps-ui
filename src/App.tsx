52cf14d8ef0de4fca4e717cf3e57fbbe2d6e6735
btk44 <btko@tuta.io>
2023-05-27 11:59:14 +0200

    styling html

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import './App.scss'
 import Router from './Router'
 import { AuthInterceptor } from './services/Interceptor'
 import store from './store/Store'
+import { useEffect } from 'react'
+
 
 function App() {
+  useEffect(() => {
+    document.documentElement.classList.add('html');
+    document.documentElement.classList.add('mobile-html');
+    document.documentElement.classList.add('light-theme');
+  }, []);
+  
   AuthInterceptor();
 
   return (
